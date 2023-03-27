32d95fc0fdff36f70b58831a8593bb9280b8034a
paikkaja <faiqaqilla2004@gmail.com>
2023-03-27 09:35:40 +0700

    v.1.3

---
+++ proses.php
@@ -0,0 +1,22 @@
+<?php
+      // Ambil nilai dari form HTML
+      $angka1 = $_POST["angka1"];
+      $angka2 = $_POST["angka2"];
+      $operator = $_POST["operator"];
+
+      // Lakukan operasi aritmatika sesuai dengan operator
+      if ($operator == "+") {
+        $hasil = $angka1 + $angka2;
+      } else if ($operator == "-") {
+        $hasil = $angka1 - $angka2;
+      } else if ($operator == "*") {
+        $hasil = $angka1 * $angka2;
+      } else if ($operator == "/") {
+        $hasil = $angka1 / $angka2;
+      } else {
+        $hasil = "Operator tidak valid";
+      }
+
+      // Tampilkan hasil operasi aritmatika
+      echo "<h1>Hasil: $hasil</h1>";
+?>
